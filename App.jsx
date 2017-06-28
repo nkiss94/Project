de0af5a51b5ec3d87df25d3fa61c4179fcd67318
nkiss94 <knick1215@gmail.com>
2017-06-27 20:56:20 -0400

    new project

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+project = new Mongo.Collection("project")
+
+export default class App extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<h1>My Projects</h1>
+				
+			</div>
+		)
+	}
+}
